414b8af1a38510e47d5d57d25328d77070b39904
Ritika Kumari Gupta <259775213+ritikakumarigupta@users.noreply.github.com>
2026-08-12 11:46:18 +0530

    Updated backend URL to Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,15 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://campussetu-fk70.onrender.com', // 👈 Render ka backend URL
 });
 
-// Request Interceptor
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 export default API;
